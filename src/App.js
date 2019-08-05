52c200bc9649777622a8f083ada2314d431cd2f6
Josh Bivens <thejoshbivens@gmail.com>
2019-08-05 16:58:27 -0600

    Add CommentList to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CommentForm from './CommentForm/CommentForm';
+import CommentList from './CommentList/CommentList';
 import './App.css';
 
 const App = () => (
   <div>
     <h2>Comments</h2>
     <CommentForm />
+    <CommentList />
   </div>
 );
 
