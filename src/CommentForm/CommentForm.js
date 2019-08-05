2a1f044d44c25f237694722707074477f2cf2bc1
Josh Bivens <thejoshbivens@gmail.com>
2019-08-05 16:36:37 -0600

    Create CommentForm folder

---
+++ src/CommentForm/CommentForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CommentForm.css';
+
+class CommentForm extends React.Component {
+	render() {
+		return (
+			<h1>CommentForm coming through!</h1>
+		)
+	}
+}
+
+export default CommentForm;
