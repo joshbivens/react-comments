dee2b9c424b69413999f15b29a24ffaad683e4b0
Josh Bivens <thejoshbivens@gmail.com>
2019-08-06 12:58:11 -0600

    Complete App Component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import CommentList from './components/CommentList/CommentList';
 import './App.css';
 
 const App = () => (
-  <div>
+  <div className="comments">
     <h2>Comments</h2>
     <CommentForm />
     <CommentList />
+    <footer>
+      Vue edition <a target="blank" href="https://codepen.io/joshbivens/pen/pYVBpG">here</a> | &#169; 2019 by <a target="blank" href="https://github.com/joshbivens">Josh Bivens</a>
+    </footer>
   </div>
 );
 
